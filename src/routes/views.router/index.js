d454b282cf5d54d1fdcec4be9d815eec8cecdc8a
Carlos7979 <carloscorredorwg@gmail.com>
2023-05-26 19:25:40 -0400

    add cart view and its logic for handle total amount

--- src/routes/views.router/index.js
+++ src/routes/views.router/index.js
@@ -5,10 +5,12 @@ const router = Router()
 const products = require('./products.view.router')
 const chat = require('./chat.view.router')
 const home = require('./home.view.router')
+const cart = require('./cart.view.router')
 
 // Configurar los routers
 router.use('/products', products)
 router.use('/chat', chat)
+router.use('/carts', cart)
 router.use('/', home)
 
 module.exports = router
