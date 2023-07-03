d2b3e1633bc76fc0cad4f6b2850205f38bb00a6f
Carlos7979 <carloscorredorwg@gmail.com>
2023-07-03 04:54:23 -0400

    add enviroment routes

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-require('dotenv').config()
 const { connect } = require('mongoose')
 const app = require('./src/app.js')
-
-const {
-    env: { PORT, MONGO_URL: url },
-    argv: [, , port = PORT || 8080]
-} = process
+const { env: { pid, MONGO_URL: url, PORT: port, mode } } = require('./src/config')
 
 ;(async () => {
     await connect(url, { useNewUrlParser: true })
     app.listen(port, () => {
-        console.log(`Backend app listening on port ${port}\nhttp://localhost:${port}`)
+        console.log(`Backend app listening on port ${port}`)
+        console.log('\x1b[36m%s\x1b[0m', `http://localhost:${port}`)
+		console.log('\x1b[33m%s\x1b[0m', `Mode: ${mode}`)
+        console.log(`pid: ${pid}`)
     })
 })()
